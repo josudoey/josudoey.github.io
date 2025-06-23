96f60e4453c9ce71e617483ab40eac3a3f312e58
Joey Fang <josudoey@gmail.com>
2025-06-23 18:13:59 +0800

    feat: component add timeline

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,11 @@
+import MDXComponents from '@theme-original/MDXComponents'
+
+import Timeline from '@site/src/components/Timeline'
+import TimelineItem from '@site/src/components/Timeline/TimelineItem'
+
+export default {
+  // Reusing the default mapping
+  ...MDXComponents,
+  Timeline,
+  TimelineItem
+}
