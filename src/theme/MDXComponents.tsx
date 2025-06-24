6ae7bbd50946b940c2aef516c065bf738d849d31
Joey Fang <josudoey@gmail.com>
2025-06-24 15:37:25 +0800

    fix: correct timeline component

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,16 +1,16 @@
-import MDXComponents from '@theme-original/MDXComponents'
+import MDXComponents from "@theme-original/MDXComponents";
 
-import Timeline from '@site/src/components/Timeline'
-import TimelineItem from '@site/src/components/Timeline/TimelineItem'
+import TimeLine from "@site/src/components/TimeLine";
+import TimeLineItem from "@site/src/components/TimeLine/TimeLineItem";
 
-import SingleTimeline from '@site/src/components/SingleTimeLine'
-import SingleTimeLineItem from '@site/src/components/SingleTimeLine/SingleTimeLineItem'
+import SingleTimeline from "@site/src/components/SingleTimeLine";
+import SingleTimeLineItem from "@site/src/components/SingleTimeLine/SingleTimeLineItem";
 
 export default {
   // Reusing the default mapping
   ...MDXComponents,
+  TimeLine,
+  TimeLineItem,
   SingleTimeline,
   SingleTimelineItem: SingleTimeLineItem,
-  Timeline,
-  TimelineItem
-}
+};
